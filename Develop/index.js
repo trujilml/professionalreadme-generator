7c4944baeea0e535411e30e5b5fa03cb07ac4155
trujilml <mickeytrj@gmail.com>
2021-11-14 01:28:28 -0600

    installed npm inquirer and added to gitignore file for node_modules

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,20 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+
+
+        []
+
+
+
+    ])
+
+
+
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
