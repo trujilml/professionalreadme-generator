e6d5090497d88cb5b282fd71487516d44636c7cf
trujilml <mickeytrj@gmail.com>
2021-11-15 12:04:29 -0600

    added few questions to index js, reformatted files for markdown and printed out readme, so far node index action is working well - implement more required questions license badges and extra tests

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// TODO: Create a function that returns a license badge based on which license is passed in - will be placed within either this doc or index.js
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown');
+const generateMarkdown = require('./Develop/utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = () => {
     return inquirer.prompt([
-      {  type: 'input',
+      {  
+        //   project title is entered first
+        type: 'input',
         name: 'title',
-        message: 'Hello! Please enter your project name. (Required)',
+        message: 'Hello! Please enter your project name. (Required)', 
         validate: projectTitle => {
             if (projectTitle) {
                 return true;
@@ -16,14 +18,31 @@ const questions = () => {
                 console.log("Please enter the title to your project!");
                 return false;
             }
+            }
         },
 
-
+    {
+        // description of project is entered
+        type: 'input',
+        name: 'description',
+        message: 'Now, can you please a provide a description of your project? (Required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log("Please enter a description for your project!");
+                return false;
+            }
+        }
     }
+])
+}
+
+    
 
 
 
-    ])
+   
 
 // should generate the following (from readme guide) - take to markdown file but keep for question prompt reference
 // project title (REQUIRED) ${data.title}
@@ -40,13 +59,22 @@ const questions = () => {
 //github ${data.github}")
 
 
+
+// TODO: Create a function to write README file 
+const writeToFile = (data) => {
+    fs.writeFile('./dist/readme.md', data, err => {
+        if (err) 
+            throw err;
+           console.log("README file has been created!");
+});
 }
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+// TODO: Create a function to initialize app - put as a promise chain functionality
+questions()
+.then(data => {console.log('Done!'); return data})
+    .then(data => {return generateMarkdown(data)})
+        .then(writeReadme => {
+            return writeToFile(writeReadme);
+        })
+        .catch(error => {console.log(error + "Whoops! An error has popped up in your code!")
+    })
